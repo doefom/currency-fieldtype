f6690c7d830869af84d801154887abcbe4a3923d
Dominik Müller <d.mueller@emplify-software.de>
2023-07-05 17:45:31 +0200

    Added top25 currencies

--- src/Fieldtypes/CurrencyFieldtype.php
+++ src/Fieldtypes/CurrencyFieldtype.php
@@ -12,8 +12,6 @@ use Statamic\Support\Arr;
 class CurrencyFieldtype extends Fieldtype
 {
 
-//    protected $icon = '<svg height="100%" stroke-miterlimit="10"style="fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;" version="1.1" viewBox="0 0 1024 1024" width="100%" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs/><g id="Ebene-1"><g opacity="1"><path d="M696.625 641.124C696.625 437.244 386.274 541.449 386.274 385.141C386.274 333.038 406.662 271.874 513.133 271.874C621.869 271.874 635.461 357.957 637.726 396.468L685.299 396.468C689.829 333.038 644.522 240.159 535.786 235.628L535.786 172.199L489.347 172.199L489.347 235.628C362.487 242.424 340.967 337.569 340.967 380.61C340.967 593.552 651.318 480.285 651.318 641.124C651.318 661.513 646.788 752.126 513.133 752.126C508.602 752.126 370.416 752.126 374.947 609.41L327.375 609.41C331.905 761.188 428.182 781.576 489.347 788.372L489.347 851.801L534.653 851.801L534.653 788.372C686.431 777.045 696.625 663.778 696.625 641.124Z" fill="#000000" fill-rule="nonzero" opacity="1" stroke="#1f2937" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="2"/><path d="M512 13.625C236.755 13.625 13.625 236.755 13.625 512C13.625 787.245 236.755 1010.38 512 1010.38C787.245 1010.38 1010.38 787.245 1010.38 512C1010.38 236.755 787.245 13.625 512 13.625Z" fill="none" opacity="1" stroke="#1f2937" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="2"/></g></g></svg>';
-
     public function icon()
     {
         return file_get_contents(__DIR__ . '/../../resources/svg/dollar.svg');
@@ -76,7 +74,9 @@ class CurrencyFieldtype extends Fieldtype
                 'instructions' => 'Select which currency you want to use for the field.',
                 'type' => 'select',
                 'default' => 'USD',
-                'options' => Currencies::$currencyList,
+                'options' => collect(Currencies::$top25)
+                    ->map(fn($item, $key) => Arr::get($item, 'name') . " ($key)")
+                    ->sortBy(fn($val) => $val),
                 'width' => 50
             ],
         ];
