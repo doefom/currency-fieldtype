99342f7e9b8cb0e7bfbe7482c870232237f637db
Casey Dwyer <casey@cubitcreative.com>
2023-10-02 21:49:54 -0500

    new sub-unit tests

---
+++ tests/Feature/SubunitStorageTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Feature;
+
+use Doefom\CurrencyFieldtype\Fieldtypes\CurrencyFieldtype;
+use Doefom\CurrencyFieldtype\Models\Currency;
+use Statamic\Facades\Site;
+use Statamic\Fields\Field;
+use Tests\TestCase;
+
+class SubunitStorageTest extends TestCase
+{
+    protected CurrencyFieldtype $currencyFieldtype;
+    protected Currency $augmented;
+
+    /**
+     * Set up a field of currency fieldtype with a value of 1234.56 and the following configuration:
+     * handle: price
+     * iso: USD
+     * subunits: true
+     * For NumberFormatter use the locale en_US.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Site::setCurrent('en_US');
+
+        $value = 123456;
+
+        // --------------------------------------------------------
+        // SET UP FIELDTYPE
+        // --------------------------------------------------------
+
+        $field = new Field('price', [
+            'iso' => 'USD',
+            'type' => 'currency',
+            'display' => 'Price',
+            'icon' => 'currency',
+            'listable' => 'hidden',
+            'instructions_position' => 'above',
+            'visibility' => 'visible',
+            'hide_display' => false,
+            'store_sub_units' => true,
+        ]);
+        $field->setValue($value);
+
+        $currencyFieldtype = new CurrencyFieldtype();
+        $currencyFieldtype->setField($field);
+
+        $this->currencyFieldtype = $currencyFieldtype;
+
+        // --------------------------------------------------------
+        // SET UP AUGMENTED INSTANCE
+        // --------------------------------------------------------
+
+        $this->augmented = $this->currencyFieldtype->augment($value);
+
+    }
+
+    public function test_pre_process()
+    {
+        $result = $this->currencyFieldtype->preProcess(123456);
+        $this->assertEquals('1,234.56', $result);
+    }
+
+    public function test_process()
+    {
+        $result = $this->currencyFieldtype->process('1,234.56');
+        $this->assertEquals(123456, $result);
+    }
+
+    public function test_pre_process_index()
+    {
+        $result = $this->currencyFieldtype->preProcessIndex(123456);
+        $this->assertEquals('$1,234.56', $result);
+    }
+
+    public function test_augmented_value()
+    {
+        $this->assertEquals(123456, $this->augmented->value);
+    }
+
+    public function test_augmented_display_value()
+    {
+        $this->assertEquals(1234.56, $this->augmented->display_value);
+    }
+
+    public function test_augmented_formatted()
+    {
+        $this->assertEquals('$1,234.56', $this->augmented->formatted);
+    }
+
+    public function test_augmented_formatted_no_symbol()
+    {
+        $this->assertEquals('1,234.56', $this->augmented->formattedNoSymbol);
+    }
+
+    public function test_augmented_iso()
+    {
+        $this->assertEquals('USD', $this->augmented->iso);
+    }
+
+    public function test_augmented_numeric_code()
+    {
+        $this->assertEquals('840', $this->augmented->numericCode);
+    }
+
+    public function test_augmented_symbol()
+    {
+        $this->assertEquals('$', $this->augmented->symbol);
+    }
+
+    public function test_augmented_append()
+    {
+        $this->assertFalse($this->augmented->append);
+    }
+
+    public function test_augmented_group_separator()
+    {
+        $this->assertEquals(',', $this->augmented->groupSeparator);
+    }
+
+    public function test_augmented_radix_point()
+    {
+        $this->assertEquals('.', $this->augmented->radixPoint);
+    }
+
+    public function test_augmented_digits()
+    {
+        $this->assertEquals(2, $this->augmented->digits);
+    }
+
+}
